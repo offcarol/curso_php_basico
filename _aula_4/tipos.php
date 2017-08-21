96563693f2dc9fda8e52452cf7e4adc6c1d34c00
Carolina <offcarolina@hotmail.com>
2017-08-21 05:58:06 +0000

    aula 4

---
+++ _aula_4/tipos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="_css/estilo.css">
+		<title></title>
+
+	</head>
+
+	<body>
+		<div>
+
+			<?php 
+				
+				$nome = "Carol";
+				$idade = "23";
+				echo "$nome tem $idade anos.";
+
+			?>
+
+		</div>
+	</body>
+
+</html>
